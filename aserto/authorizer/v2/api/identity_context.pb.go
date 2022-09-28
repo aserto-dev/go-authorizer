2c685f230570e30f67faaf89f3c234465de92f21
carabasdaniel <dani@aserto.com>
2022-09-28 18:30:45 +0300

    Update paths in go-authorizer

--- aserto/authorizer/v2/api/identity_context.pb.go
+++ aserto/authorizer/v2/api/identity_context.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.28.0
 // 	protoc        (unknown)
-// source: aserto/authorizer/api/v2/identity_context.proto
+// source: aserto/authorizer/v2/api/identity_context.proto
 
 package api
 
@@ -61,11 +61,11 @@ func (x IdentityType) String() string {
 }
 
 func (IdentityType) Descriptor() protoreflect.EnumDescriptor {
-	return file_aserto_authorizer_api_v2_identity_context_proto_enumTypes[0].Descriptor()
+	return file_aserto_authorizer_v2_api_identity_context_proto_enumTypes[0].Descriptor()
 }
 
 func (IdentityType) Type() protoreflect.EnumType {
-	return &file_aserto_authorizer_api_v2_identity_context_proto_enumTypes[0]
+	return &file_aserto_authorizer_v2_api_identity_context_proto_enumTypes[0]
 }
 
 func (x IdentityType) Number() protoreflect.EnumNumber {
@@ -74,7 +74,7 @@ func (x IdentityType) Number() protoreflect.EnumNumber {
 
 // Deprecated: Use IdentityType.Descriptor instead.
 func (IdentityType) EnumDescriptor() ([]byte, []int) {
-	return file_aserto_authorizer_api_v2_identity_context_proto_rawDescGZIP(), []int{0}
+	return file_aserto_authorizer_v2_api_identity_context_proto_rawDescGZIP(), []int{0}
 }
 
 type IdentityContext struct {
@@ -83,13 +83,13 @@ type IdentityContext struct {
 	unknownFields protoimpl.UnknownFields
 
 	Identity string       `protobuf:"bytes,1,opt,name=identity,proto3" json:"identity,omitempty"`
-	Type     IdentityType `protobuf:"varint,2,opt,name=type,proto3,enum=aserto.authorizer.api.v2.IdentityType" json:"type,omitempty"`
+	Type     IdentityType `protobuf:"varint,2,opt,name=type,proto3,enum=aserto.authorizer.v2.api.IdentityType" json:"type,omitempty"`
 }
 
 func (x *IdentityContext) Reset() {
 	*x = IdentityContext{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_aserto_authorizer_api_v2_identity_context_proto_msgTypes[0]
+		mi := &file_aserto_authorizer_v2_api_identity_context_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -102,7 +102,7 @@ func (x *IdentityContext) String() string {
 func (*IdentityContext) ProtoMessage() {}
 
 func (x *IdentityContext) ProtoReflect() protoreflect.Message {
-	mi := &file_aserto_authorizer_api_v2_identity_context_proto_msgTypes[0]
+	mi := &file_aserto_authorizer_v2_api_identity_context_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -115,7 +115,7 @@ func (x *IdentityContext) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use IdentityContext.ProtoReflect.Descriptor instead.
 func (*IdentityContext) Descriptor() ([]byte, []int) {
-	return file_aserto_authorizer_api_v2_identity_context_proto_rawDescGZIP(), []int{0}
+	return file_aserto_authorizer_v2_api_identity_context_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *IdentityContext) GetIdentity() string {
@@ -132,20 +132,20 @@ func (x *IdentityContext) GetType() IdentityType {
 	return IdentityType_IDENTITY_TYPE_UNKNOWN
 }
 
-var File_aserto_authorizer_api_v2_identity_context_proto protoreflect.FileDescriptor
+var File_aserto_authorizer_v2_api_identity_context_proto protoreflect.FileDescriptor
 
-var file_aserto_authorizer_api_v2_identity_context_proto_rawDesc = []byte{
+var file_aserto_authorizer_v2_api_identity_context_proto_rawDesc = []byte{
 	0x0a, 0x2f, 0x61, 0x73, 0x65, 0x72, 0x74, 0x6f, 0x2f, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69,
-	0x7a, 0x65, 0x72, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x76, 0x32, 0x2f, 0x69, 0x64, 0x65, 0x6e, 0x74,
+	0x7a, 0x65, 0x72, 0x2f, 0x76, 0x32, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x69, 0x64, 0x65, 0x6e, 0x74,
 	0x69, 0x74, 0x79, 0x5f, 0x63, 0x6f, 0x6e, 0x74, 0x65, 0x78, 0x74, 0x2e, 0x70, 0x72, 0x6f, 0x74,
 	0x6f, 0x12, 0x18, 0x61, 0x73, 0x65, 0x72, 0x74, 0x6f, 0x2e, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72,
-	0x69, 0x7a, 0x65, 0x72, 0x2e, 0x61, 0x70, 0x69, 0x2e, 0x76, 0x32, 0x22, 0x69, 0x0a, 0x0f, 0x49,
+	0x69, 0x7a, 0x65, 0x72, 0x2e, 0x76, 0x32, 0x2e, 0x61, 0x70, 0x69, 0x22, 0x69, 0x0a, 0x0f, 0x49,
 	0x64, 0x65, 0x6e, 0x74, 0x69, 0x74, 0x79, 0x43, 0x6f, 0x6e, 0x74, 0x65, 0x78, 0x74, 0x12, 0x1a,
 	0x0a, 0x08, 0x69, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x74, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
 	0x52, 0x08, 0x69, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x74, 0x79, 0x12, 0x3a, 0x0a, 0x04, 0x74, 0x79,
 	0x70, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0e, 0x32, 0x26, 0x2e, 0x61, 0x73, 0x65, 0x72, 0x74,
-	0x6f, 0x2e, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69, 0x7a, 0x65, 0x72, 0x2e, 0x61, 0x70, 0x69,
-	0x2e, 0x76, 0x32, 0x2e, 0x49, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x74, 0x79, 0x54, 0x79, 0x70, 0x65,
+	0x6f, 0x2e, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69, 0x7a, 0x65, 0x72, 0x2e, 0x76, 0x32, 0x2e,
+	0x61, 0x70, 0x69, 0x2e, 0x49, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x74, 0x79, 0x54, 0x79, 0x70, 0x65,
 	0x52, 0x04, 0x74, 0x79, 0x70, 0x65, 0x2a, 0x6f, 0x0a, 0x0c, 0x49, 0x64, 0x65, 0x6e, 0x74, 0x69,
 	0x74, 0x79, 0x54, 0x79, 0x70, 0x65, 0x12, 0x19, 0x0a, 0x15, 0x49, 0x44, 0x45, 0x4e, 0x54, 0x49,
 	0x54, 0x59, 0x5f, 0x54, 0x59, 0x50, 0x45, 0x5f, 0x55, 0x4e, 0x4b, 0x4e, 0x4f, 0x57, 0x4e, 0x10,
@@ -157,31 +157,31 @@ var file_aserto_authorizer_api_v2_identity_context_proto_rawDesc = []byte{
 	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x61, 0x73, 0x65, 0x72, 0x74, 0x6f, 0x2d, 0x64, 0x65, 0x76,
 	0x2f, 0x67, 0x6f, 0x2d, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69, 0x7a, 0x65, 0x72, 0x2f, 0x61,
 	0x73, 0x65, 0x72, 0x74, 0x6f, 0x2f, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69, 0x7a, 0x65, 0x72,
-	0x2f, 0x61, 0x70, 0x69, 0x2f, 0x76, 0x32, 0x3b, 0x61, 0x70, 0x69, 0xaa, 0x02, 0x18, 0x41, 0x73,
+	0x2f, 0x76, 0x32, 0x2f, 0x61, 0x70, 0x69, 0x3b, 0x61, 0x70, 0x69, 0xaa, 0x02, 0x18, 0x41, 0x73,
 	0x65, 0x72, 0x74, 0x6f, 0x2e, 0x41, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69, 0x7a, 0x65, 0x72, 0x2e,
-	0x41, 0x50, 0x49, 0x2e, 0x56, 0x32, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x56, 0x32, 0x2e, 0x41, 0x50, 0x49, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_aserto_authorizer_api_v2_identity_context_proto_rawDescOnce sync.Once
-	file_aserto_authorizer_api_v2_identity_context_proto_rawDescData = file_aserto_authorizer_api_v2_identity_context_proto_rawDesc
+	file_aserto_authorizer_v2_api_identity_context_proto_rawDescOnce sync.Once
+	file_aserto_authorizer_v2_api_identity_context_proto_rawDescData = file_aserto_authorizer_v2_api_identity_context_proto_rawDesc
 )
 
-func file_aserto_authorizer_api_v2_identity_context_proto_rawDescGZIP() []byte {
-	file_aserto_authorizer_api_v2_identity_context_proto_rawDescOnce.Do(func() {
-		file_aserto_authorizer_api_v2_identity_context_proto_rawDescData = protoimpl.X.CompressGZIP(file_aserto_authorizer_api_v2_identity_context_proto_rawDescData)
+func file_aserto_authorizer_v2_api_identity_context_proto_rawDescGZIP() []byte {
+	file_aserto_authorizer_v2_api_identity_context_proto_rawDescOnce.Do(func() {
+		file_aserto_authorizer_v2_api_identity_context_proto_rawDescData = protoimpl.X.CompressGZIP(file_aserto_authorizer_v2_api_identity_context_proto_rawDescData)
 	})
-	return file_aserto_authorizer_api_v2_identity_context_proto_rawDescData
+	return file_aserto_authorizer_v2_api_identity_context_proto_rawDescData
 }
 
-var file_aserto_authorizer_api_v2_identity_context_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
-var file_aserto_authorizer_api_v2_identity_context_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_aserto_authorizer_api_v2_identity_context_proto_goTypes = []interface{}{
-	(IdentityType)(0),       // 0: aserto.authorizer.api.v2.IdentityType
-	(*IdentityContext)(nil), // 1: aserto.authorizer.api.v2.IdentityContext
+var file_aserto_authorizer_v2_api_identity_context_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
+var file_aserto_authorizer_v2_api_identity_context_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_aserto_authorizer_v2_api_identity_context_proto_goTypes = []interface{}{
+	(IdentityType)(0),       // 0: aserto.authorizer.v2.api.IdentityType
+	(*IdentityContext)(nil), // 1: aserto.authorizer.v2.api.IdentityContext
 }
-var file_aserto_authorizer_api_v2_identity_context_proto_depIdxs = []int32{
-	0, // 0: aserto.authorizer.api.v2.IdentityContext.type:type_name -> aserto.authorizer.api.v2.IdentityType
+var file_aserto_authorizer_v2_api_identity_context_proto_depIdxs = []int32{
+	0, // 0: aserto.authorizer.v2.api.IdentityContext.type:type_name -> aserto.authorizer.v2.api.IdentityType
 	1, // [1:1] is the sub-list for method output_type
 	1, // [1:1] is the sub-list for method input_type
 	1, // [1:1] is the sub-list for extension type_name
@@ -189,13 +189,13 @@ var file_aserto_authorizer_api_v2_identity_context_proto_depIdxs = []int32{
 	0, // [0:1] is the sub-list for field type_name
 }
 
-func init() { file_aserto_authorizer_api_v2_identity_context_proto_init() }
-func file_aserto_authorizer_api_v2_identity_context_proto_init() {
-	if File_aserto_authorizer_api_v2_identity_context_proto != nil {
+func init() { file_aserto_authorizer_v2_api_identity_context_proto_init() }
+func file_aserto_authorizer_v2_api_identity_context_proto_init() {
+	if File_aserto_authorizer_v2_api_identity_context_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_aserto_authorizer_api_v2_identity_context_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_aserto_authorizer_v2_api_identity_context_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*IdentityContext); i {
 			case 0:
 				return &v.state
@@ -212,19 +212,19 @@ func file_aserto_authorizer_api_v2_identity_context_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_aserto_authorizer_api_v2_identity_context_proto_rawDesc,
+			RawDescriptor: file_aserto_authorizer_v2_api_identity_context_proto_rawDesc,
 			NumEnums:      1,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_aserto_authorizer_api_v2_identity_context_proto_goTypes,
-		DependencyIndexes: file_aserto_authorizer_api_v2_identity_context_proto_depIdxs,
-		EnumInfos:         file_aserto_authorizer_api_v2_identity_context_proto_enumTypes,
-		MessageInfos:      file_aserto_authorizer_api_v2_identity_context_proto_msgTypes,
+		GoTypes:           file_aserto_authorizer_v2_api_identity_context_proto_goTypes,
+		DependencyIndexes: file_aserto_authorizer_v2_api_identity_context_proto_depIdxs,
+		EnumInfos:         file_aserto_authorizer_v2_api_identity_context_proto_enumTypes,
+		MessageInfos:      file_aserto_authorizer_v2_api_identity_context_proto_msgTypes,
 	}.Build()
-	File_aserto_authorizer_api_v2_identity_context_proto = out.File
-	file_aserto_authorizer_api_v2_identity_context_proto_rawDesc = nil
-	file_aserto_authorizer_api_v2_identity_context_proto_goTypes = nil
-	file_aserto_authorizer_api_v2_identity_context_proto_depIdxs = nil
+	File_aserto_authorizer_v2_api_identity_context_proto = out.File
+	file_aserto_authorizer_v2_api_identity_context_proto_rawDesc = nil
+	file_aserto_authorizer_v2_api_identity_context_proto_goTypes = nil
+	file_aserto_authorizer_v2_api_identity_context_proto_depIdxs = nil
 }
--- aserto/authorizer/v2/api/policy_context.pb.go
+++ aserto/authorizer/v2/api/policy_context.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.28.0
 // 	protoc        (unknown)
-// source: aserto/authorizer/api/v2/policy_context.proto
+// source: aserto/authorizer/v2/api/policy_context.proto
 
 package api
 
@@ -34,7 +34,7 @@ type PolicyContext struct {
 func (x *PolicyContext) Reset() {
 	*x = PolicyContext{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_aserto_authorizer_api_v2_policy_context_proto_msgTypes[0]
+		mi := &file_aserto_authorizer_v2_api_policy_context_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -47,7 +47,7 @@ func (x *PolicyContext) String() string {
 func (*PolicyContext) ProtoMessage() {}
 
 func (x *PolicyContext) ProtoReflect() protoreflect.Message {
-	mi := &file_aserto_authorizer_api_v2_policy_context_proto_msgTypes[0]
+	mi := &file_aserto_authorizer_v2_api_policy_context_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -60,7 +60,7 @@ func (x *PolicyContext) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use PolicyContext.ProtoReflect.Descriptor instead.
 func (*PolicyContext) Descriptor() ([]byte, []int) {
-	return file_aserto_authorizer_api_v2_policy_context_proto_rawDescGZIP(), []int{0}
+	return file_aserto_authorizer_v2_api_policy_context_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *PolicyContext) GetName() string {
@@ -91,14 +91,14 @@ func (x *PolicyContext) GetDecisions() []string {
 	return nil
 }
 
-var File_aserto_authorizer_api_v2_policy_context_proto protoreflect.FileDescriptor
+var File_aserto_authorizer_v2_api_policy_context_proto protoreflect.FileDescriptor
 
-var file_aserto_authorizer_api_v2_policy_context_proto_rawDesc = []byte{
+var file_aserto_authorizer_v2_api_policy_context_proto_rawDesc = []byte{
 	0x0a, 0x2d, 0x61, 0x73, 0x65, 0x72, 0x74, 0x6f, 0x2f, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69,
-	0x7a, 0x65, 0x72, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x76, 0x32, 0x2f, 0x70, 0x6f, 0x6c, 0x69, 0x63,
+	0x7a, 0x65, 0x72, 0x2f, 0x76, 0x32, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x70, 0x6f, 0x6c, 0x69, 0x63,
 	0x79, 0x5f, 0x63, 0x6f, 0x6e, 0x74, 0x65, 0x78, 0x74, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
 	0x18, 0x61, 0x73, 0x65, 0x72, 0x74, 0x6f, 0x2e, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69, 0x7a,
-	0x65, 0x72, 0x2e, 0x61, 0x70, 0x69, 0x2e, 0x76, 0x32, 0x22, 0x7c, 0x0a, 0x0d, 0x50, 0x6f, 0x6c,
+	0x65, 0x72, 0x2e, 0x76, 0x32, 0x2e, 0x61, 0x70, 0x69, 0x22, 0x7c, 0x0a, 0x0d, 0x50, 0x6f, 0x6c,
 	0x69, 0x63, 0x79, 0x43, 0x6f, 0x6e, 0x74, 0x65, 0x78, 0x74, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61,
 	0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x25,
 	0x0a, 0x0e, 0x69, 0x6e, 0x73, 0x74, 0x61, 0x6e, 0x63, 0x65, 0x5f, 0x6c, 0x61, 0x62, 0x65, 0x6c,
@@ -110,28 +110,28 @@ var file_aserto_authorizer_api_v2_policy_context_proto_rawDesc = []byte{
 	0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x61, 0x73, 0x65, 0x72, 0x74, 0x6f, 0x2d, 0x64, 0x65, 0x76,
 	0x2f, 0x67, 0x6f, 0x2d, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69, 0x7a, 0x65, 0x72, 0x2f, 0x61,
 	0x73, 0x65, 0x72, 0x74, 0x6f, 0x2f, 0x61, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69, 0x7a, 0x65, 0x72,
-	0x2f, 0x61, 0x70, 0x69, 0x2f, 0x76, 0x32, 0x3b, 0x61, 0x70, 0x69, 0xaa, 0x02, 0x18, 0x41, 0x73,
+	0x2f, 0x76, 0x32, 0x2f, 0x61, 0x70, 0x69, 0x3b, 0x61, 0x70, 0x69, 0xaa, 0x02, 0x18, 0x41, 0x73,
 	0x65, 0x72, 0x74, 0x6f, 0x2e, 0x41, 0x75, 0x74, 0x68, 0x6f, 0x72, 0x69, 0x7a, 0x65, 0x72, 0x2e,
-	0x41, 0x50, 0x49, 0x2e, 0x56, 0x32, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x56, 0x32, 0x2e, 0x41, 0x50, 0x49, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_aserto_authorizer_api_v2_policy_context_proto_rawDescOnce sync.Once
-	file_aserto_authorizer_api_v2_policy_context_proto_rawDescData = file_aserto_authorizer_api_v2_policy_context_proto_rawDesc
+	file_aserto_authorizer_v2_api_policy_context_proto_rawDescOnce sync.Once
+	file_aserto_authorizer_v2_api_policy_context_proto_rawDescData = file_aserto_authorizer_v2_api_policy_context_proto_rawDesc
 )
 
-func file_aserto_authorizer_api_v2_policy_context_proto_rawDescGZIP() []byte {
-	file_aserto_authorizer_api_v2_policy_context_proto_rawDescOnce.Do(func() {
-		file_aserto_authorizer_api_v2_policy_context_proto_rawDescData = protoimpl.X.CompressGZIP(file_aserto_authorizer_api_v2_policy_context_proto_rawDescData)
+func file_aserto_authorizer_v2_api_policy_context_proto_rawDescGZIP() []byte {
+	file_aserto_authorizer_v2_api_policy_context_proto_rawDescOnce.Do(func() {
+		file_aserto_authorizer_v2_api_policy_context_proto_rawDescData = protoimpl.X.CompressGZIP(file_aserto_authorizer_v2_api_policy_context_proto_rawDescData)
 	})
-	return file_aserto_authorizer_api_v2_policy_context_proto_rawDescData
+	return file_aserto_authorizer_v2_api_policy_context_proto_rawDescData
 }
 
-var file_aserto_authorizer_api_v2_policy_context_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_aserto_authorizer_api_v2_policy_context_proto_goTypes = []interface{}{
-	(*PolicyContext)(nil), // 0: aserto.authorizer.api.v2.PolicyContext
+var file_aserto_authorizer_v2_api_policy_context_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_aserto_authorizer_v2_api_policy_context_proto_goTypes = []interface{}{
+	(*PolicyContext)(nil), // 0: aserto.authorizer.v2.api.PolicyContext
 }
-var file_aserto_authorizer_api_v2_policy_context_proto_depIdxs = []int32{
+var file_aserto_authorizer_v2_api_policy_context_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -139,13 +139,13 @@ var file_aserto_authorizer_api_v2_policy_context_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_aserto_authorizer_api_v2_policy_context_proto_init() }
-func file_aserto_authorizer_api_v2_policy_context_proto_init() {
-	if File_aserto_authorizer_api_v2_policy_context_proto != nil {
+func init() { file_aserto_authorizer_v2_api_policy_context_proto_init() }
+func file_aserto_authorizer_v2_api_policy_context_proto_init() {
+	if File_aserto_authorizer_v2_api_policy_context_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_aserto_authorizer_api_v2_policy_context_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_aserto_authorizer_v2_api_policy_context_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*PolicyContext); i {
 			case 0:
 				return &v.state
@@ -162,18 +162,18 @@ func file_aserto_authorizer_api_v2_policy_context_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_aserto_authorizer_api_v2_policy_context_proto_rawDesc,
+			RawDescriptor: file_aserto_authorizer_v2_api_policy_context_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_aserto_authorizer_api_v2_policy_context_proto_goTypes,
-		DependencyIndexes: file_aserto_authorizer_api_v2_policy_context_proto_depIdxs,
-		MessageInfos:      file_aserto_authorizer_api_v2_policy_context_proto_msgTypes,
+		GoTypes:           file_aserto_authorizer_v2_api_policy_context_proto_goTypes,
+		DependencyIndexes: file_aserto_authorizer_v2_api_policy_context_proto_depIdxs,
+		MessageInfos:      file_aserto_authorizer_v2_api_policy_context_proto_msgTypes,
 	}.Build()
-	File_aserto_authorizer_api_v2_policy_context_proto = out.File
-	file_aserto_authorizer_api_v2_policy_context_proto_rawDesc = nil
-	file_aserto_authorizer_api_v2_policy_context_proto_goTypes = nil
-	file_aserto_authorizer_api_v2_policy_context_proto_depIdxs = nil
+	File_aserto_authorizer_v2_api_policy_context_proto = out.File
+	file_aserto_authorizer_v2_api_policy_context_proto_rawDesc = nil
+	file_aserto_authorizer_v2_api_policy_context_proto_goTypes = nil
+	file_aserto_authorizer_v2_api_policy_context_proto_depIdxs = nil
 }
