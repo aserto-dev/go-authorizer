e1eddeaae24f5f95eaee72dde017c467ce52ef5b
Gert Drapers <gert.drapers@live.com>
2024-04-18 15:44:06 -0700

    cleanup CI & deps (#11)
    * cleanup CI & deps
    
    * rm mage

--- go.mod
+++ go.mod
@@ -3,23 +3,21 @@ module github.com/aserto-dev/go-authorizer
 go 1.19
 
 require (
-	github.com/aserto-dev/errors v0.0.6
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.0
+	github.com/aserto-dev/errors v0.0.7
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1
 	github.com/magefile/mage v1.15.0
-	google.golang.org/genproto/googleapis/api v0.0.0-20230913181813-007df8e322eb
-	google.golang.org/grpc v1.58.2
-	google.golang.org/protobuf v1.31.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20240311173647-c811ad7063a7
+	google.golang.org/grpc v1.63.2
+	google.golang.org/protobuf v1.33.0
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/rs/zerolog v1.31.0 // indirect
-	golang.org/x/net v0.15.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	google.golang.org/genproto v0.0.0-20230920204549-e6e6cdab5c13 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230920183334-c177e329c48b // indirect
+	github.com/rs/zerolog v1.32.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240311173647-c811ad7063a7 // indirect
 )
--- pkg/aerr/errors.go
+++ pkg/aerr/errors.go
@@ -8,35 +8,35 @@ import (
 )
 
 var (
-	// Means no tenant id was found in the current context
+	// Means no tenant id was found in the current context.
 	ErrNoTenantID = newErr("E30001", codes.InvalidArgument, http.StatusBadRequest, "no tenant id specified")
-	// Means the tenant id is not valid
+	// Means the tenant id is not valid.
 	ErrInvalidTenantID = newErr("E30002", codes.InvalidArgument, http.StatusBadRequest, "invalid tenant id")
 	// The asked-for runtime is not yet available, but will likely be in the future.
 	ErrRuntimeLoading = newErr("E30003", codes.Unavailable, http.StatusTooEarly, "runtime has not yet loaded")
-	// Returned if a policy id is not found in the database
+	// Returned if a policy id is not found in the database.
 	ErrPolicyNotFound = newErr("E10004", codes.NotFound, http.StatusNotFound, "policy not found")
-	// Returned if a policy id is invalid
+	// Returned if a policy id is invalid.
 	ErrInvalidPolicyID = newErr("E30005", codes.InvalidArgument, http.StatusBadRequest, "invalid policy id")
-	// Return if a user already exists
+	// Return if a user already exists.
 	ErrUserAlreadyExists = newErr("E30006", codes.AlreadyExists, http.StatusConflict, "user already exists")
-	// Returned when authentication has failed or is not possible
+	// Returned when authentication has failed or is not possible.
 	ErrAuthenticationFailed = newErr("E30007", codes.Unauthenticated, http.StatusUnauthorized, "authentication failed")
-	// Returned when a given parameter is incorrect (wrong format, value or type)
+	// Returned when a given parameter is incorrect (wrong format, value or type).
 	ErrInvalidArgument = newErr("E30008", codes.InvalidArgument, http.StatusBadRequest, "invalid argument")
-	// Return if a user is not found
+	// Return if a user is not found.
 	ErrUserNotFound = newErr("E30009", codes.NotFound, http.StatusNotFound, "user not found")
-	// Returned when a runtime query has an error
+	// Returned when a runtime query has an error.
 	ErrBadQuery = newErr("E30010", codes.InvalidArgument, http.StatusBadRequest, "invalid query")
-	// Returned when a decision is invalid
+	// Returned when a decision is invalid.
 	ErrInvalidDecision = newErr("E30011", codes.InvalidArgument, http.StatusBadRequest, "invalid decision")
-	// Returned when a runtime failed to load
+	// Returned when a runtime failed to load.
 	ErrBadRuntime = newErr("E30012", codes.Unavailable, http.StatusServiceUnavailable, "runtime loading failed")
-	// Returned if object object id is not found in the directory
+	// Returned if object object id is not found in the directory.
 	ErrDirectoryObjectNotFound = newErr("E30013", codes.NotFound, http.StatusNotFound, "directory object not found")
 	// Returned if the loaded policy is invalid.
 	ErrInvalidPolicy = newErr("E30014", codes.Internal, http.StatusInternalServerError, "invalid policy")
-	// Returned when authorization has failed or is not possible
+	// Returned when authorization has failed or is not possible.
 	ErrAuthorizationFailed = newErr("E30015", codes.PermissionDenied, http.StatusUnauthorized, "authorization failed")
 )
 
